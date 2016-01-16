4b8db6965e4094ca0b53be90deaf513705e194a1
t <t@113.35.171.177>
2016-01-16 21:07:19 +0900

    added a constructor

--- src/tmcit/freedom/Algorithm/SA/SA.java
+++ src/tmcit/freedom/Algorithm/SA/SA.java
@@ -1,7 +1,15 @@
 package tmcit.freedom.Algorithm.SA;
 
 public class SA {
-	public SA(){
-		
+	private Problem prob;
+	private Answer ans;
+	public SA(Problem prob,Answer ans){
+		this.prob = prob;
+		this.ans = ans;
+	}
+	
+	public boolean start(){
+		//////solver start
+		return true;
 	}
 }
